9eb18886280bb034683eaaaeeccd33c0e53b4db2
eeue56 <enalicho@gmail.com>
2015-09-12 05:03:15 +0100

    trim thigns

--- Views.elm
+++ Views.elm
@@ -1,12 +1,12 @@
 module Views where
 
 import Html exposing (Html, Attribute, div, textarea, button, fromElement, text)
-import Html.Attributes exposing (placeholder, value)
+import Html.Attributes exposing (placeholder, value, hidden)
 
 import Html.Events exposing (keyCode, on, targetValue, onClick)
 
 import Json.Decode as Json
-
+import String
 import Model exposing (..)
 import Drawing exposing (..)
 
@@ -33,7 +33,7 @@ commandsTextarea address model =
           ] [],
       button [ onClick address Enter ] [ text "Run program" ],
       button [ onClick address Reset ] [ text "Reset" ],
-      textarea [ value <| model.errorMessage ] [ ]
+      textarea [ value <| String.trim model.errorMessage, hidden <| String.trim model.errorMessage == "" ] [ ]
     ]
     
 view address model =
