977356fa2010288e9fb3df8dd5f9e261d56db35d
eeue56 <enalicho@gmail.com>
2015-09-13 15:23:09 +0100

    move command suggestions out and hide when no suggestions

--- src/Parser.elm
+++ src/Parser.elm
@@ -22,6 +22,14 @@ compileError : Argument -> Model -> Model
 compileError messages model =
   { model | errorMessage <- String.join "\n" <| (model.errorMessage) :: messages }
 
+commandSuggestions : CommandLibrary -> String -> List String
+commandSuggestions dict command = 
+  List.map fst
+  <| List.take 5
+  <| List.sortBy (snd) 
+  <| List.filter (\(_,d) -> d < 3) 
+  <| List.map (\x -> (x,levenshtein command x)) (Dict.keys dict)
+
 {-|
 add a command not found error to the error log
 -}
@@ -30,12 +38,13 @@ commandNotFound dict command  =
   let
     helpWarning = 
       if (List.length <| String.split " " command) > 2 then "\nMaybe you forgot a $?"
-      else "\nMaybe you meant: \n" ++ (String.join "\n" 
-                                        <| List.map fst
-                                        <| List.take 5
-                                        <| List.sortBy (snd) 
-                                        <| List.filter (\(_,d) -> d < 3) 
-                                        <| List.map (\x -> (x,levenshtein command x)) (Dict.keys dict))
+      else 
+        let
+          suggestions = commandSuggestions dict command
+        in
+          if List.isEmpty suggestions then "" 
+          else "\nMaybe you meant: \n" ++ (String.join "\n" suggestions)
+                                        
   in
     CompileError ["command not found: " ++ command ++ helpWarning]
 
