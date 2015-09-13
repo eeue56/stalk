92306e5428b08497237a5c93a8a5901721d3e6da
eeue56 <enalicho@gmail.com>
2015-09-13 15:18:00 +0100

    move leven out and limit number of suggestions

--- src/Parser.elm
+++ src/Parser.elm
@@ -7,6 +7,7 @@ import String
 import Debug exposing (log)
 
 import Model exposing (..)
+import Utils exposing (levenshtein)
 
 {-|
 TODO: seperate runtimeErrors from compiletime errors
@@ -21,44 +22,6 @@ compileError : Argument -> Model -> Model
 compileError messages model =
   { model | errorMessage <- String.join "\n" <| (model.errorMessage) :: messages }
 
-
-levenshtein : String -> String -> Int
-levenshtein s1' s2' =
-  let
-    unsafeGet i j m = case Matrix.get i j m of Just v -> v
-    unsafeConcatV r m = case Matrix.concatVertical r m of Just v -> v
-    unsafeConcatH c m = case Matrix.concatHorizontal c m of Just v -> v
-    unsafeFromList xs = case Matrix.fromList xs of Just v -> v
-    s1 = Array.fromList <| String.toList s1'
-    s2 = Array.fromList <| String.toList s2'
-    l1 = Array.length s1
-    l2 = Array.length s2
-    cost i j = if Array.get (i-1) s1 == Array.get (j-1) s2 then 0 else 1
-    levInversion i j m = if i > 1 && 
-                       j > 1 && 
-                       Array.get (i-1) s1 == Array.get (j-2) s2 &&
-                       Array.get (j-2) s1 == Array.get (j-1) s2
-                    then min (levStep i j m) (unsafeGet (i-2) (j-2) m + 1)
-                    else levStep i j m
-
-    levStep : Int -> Int -> Matrix.Matrix Int -> Int
-    levStep i j m = case List.minimum [ unsafeGet (i-1) j m + 1
-                                      , unsafeGet i (j-1) m + 1
-                                      , unsafeGet (i-1) (j-1) m + (cost i j) ]
-                    of Just v -> v
-
-    init : Matrix.Matrix Int
-    init = unsafeConcatH
-            (unsafeFromList <| List.map (\x->[x]) [0..l2]) <|
-            unsafeConcatV
-                (unsafeFromList [[1..l1]])
-                (Matrix.repeat l1 l2 0)
-
-    step : Int -> Matrix.Matrix Int -> Matrix.Matrix Int
-    step i acc = List.foldl (\j m -> Matrix.set i j (levInversion i j m) m) acc [1..l2]
-  in
-    unsafeGet l1 l2 (List.foldl step init [1..l1])
-
 {-|
 add a command not found error to the error log
 -}
@@ -69,6 +32,7 @@ commandNotFound dict command  =
       if (List.length <| String.split " " command) > 2 then "\nMaybe you forgot a $?"
       else "\nMaybe you meant: \n" ++ (String.join "\n" 
                                         <| List.map fst
+                                        <| List.take 5
                                         <| List.sortBy (snd) 
                                         <| List.filter (\(_,d) -> d < 3) 
                                         <| List.map (\x -> (x,levenshtein command x)) (Dict.keys dict))
--- src/Utils.elm
+++ src/Utils.elm
@@ -3,6 +3,8 @@ module Utils where
 import String
 import Debug exposing (log)
 import Color exposing (Color, black, red, green, blue, rgb)
+import Matrix
+import Array
 
 alwaysOkInt : String -> Int
 alwaysOkInt v = 
@@ -20,3 +22,42 @@ rgbFromList vals =
     
   in
     rgb r g b
+
+
+
+levenshtein : String -> String -> Int
+levenshtein s1' s2' =
+  let
+    unsafeGet i j m = case Matrix.get i j m of Just v -> v
+    unsafeConcatV r m = case Matrix.concatVertical r m of Just v -> v
+    unsafeConcatH c m = case Matrix.concatHorizontal c m of Just v -> v
+    unsafeFromList xs = case Matrix.fromList xs of Just v -> v
+    s1 = Array.fromList <| String.toList s1'
+    s2 = Array.fromList <| String.toList s2'
+    l1 = Array.length s1
+    l2 = Array.length s2
+    cost i j = if Array.get (i-1) s1 == Array.get (j-1) s2 then 0 else 1
+    levInversion i j m = if i > 1 && 
+                       j > 1 && 
+                       Array.get (i-1) s1 == Array.get (j-2) s2 &&
+                       Array.get (j-2) s1 == Array.get (j-1) s2
+                    then min (levStep i j m) (unsafeGet (i-2) (j-2) m + 1)
+                    else levStep i j m
+
+    levStep : Int -> Int -> Matrix.Matrix Int -> Int
+    levStep i j m = case List.minimum [ unsafeGet (i-1) j m + 1
+                                      , unsafeGet i (j-1) m + 1
+                                      , unsafeGet (i-1) (j-1) m + (cost i j) ]
+                    of Just v -> v
+
+    init : Matrix.Matrix Int
+    init = unsafeConcatH
+            (unsafeFromList <| List.map (\x->[x]) [0..l2]) <|
+            unsafeConcatV
+                (unsafeFromList [[1..l1]])
+                (Matrix.repeat l1 l2 0)
+
+    step : Int -> Matrix.Matrix Int -> Matrix.Matrix Int
+    step i acc = List.foldl (\j m -> Matrix.set i j (levInversion i j m) m) acc [1..l2]
+  in
+    unsafeGet l1 l2 (List.foldl step init [1..l1])
