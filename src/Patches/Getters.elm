4d33a6890d52fccfdb79be0c023b1fb6932443d6
eeue56 <enalicho@gmail.com>
2015-09-14 04:30:59 +0100

    add a generalized getter for patches

--- src/Patches/Getters.elm
+++ src/Patches/Getters.elm
@@ -23,74 +23,49 @@ patchAt args model =
           Nothing -> runtimeError ["decoding failed!"] model
       Nothing -> incorrectCoords args model
 
+builtinPatchGetter : (Patch -> Model -> Model) -> Argument -> Model -> Model 
+builtinPatchGetter getter args model = 
+  if List.length args == 2 then
+    let
+      (i, j) = getCoords args model
+    in
+      case Matrix.get i j model.patches of
+        Just v -> getter v model
+        Nothing -> incorrectCoords args model
+  else 
+    case List.head args of
+      Just p -> 
+        case patchFromString p of
+          Just v -> getter v model
+          Nothing -> runtimeError ["Failed to decode patch " ++ p] model
+      Nothing -> runtimeError ["Not enough arguments!"] model
+
 {-|
 get the pcolor of a patch at i, j and push it to stack 
 -}
 pcolorOf : Argument -> Model -> Model
-pcolorOf args model = 
-  let
-    (i, j) = getCoords args model
-  in
-    case Matrix.get i j model.patches of
-      Just v -> List.foldl Stack.push model <| patchColorAsString v
-      Nothing -> incorrectCoords args model
+pcolorOf = 
+  builtinPatchGetter (\v model -> List.foldl Stack.push model <| patchColorAsString v)
+
 
 {-|
 get the pxcor of a patch at i, j and push it to stack 
 -}
 pxcorOf : Argument -> Model -> Model
-pxcorOf args model =
-  let
-    (i, j) = getCoords args model
-  in
-    if List.length args == 2 then
-      case Matrix.get i j model.patches of
-        Just v -> Stack.push (toString  v.pxcor) model
-        Nothing -> incorrectCoords args model
-    else 
-      case List.head args of
-        Just p -> 
-          case patchFromString p of
-            Just v -> Stack.push (toString v.pxcor) model
-            Nothing -> runtimeError ["Failed to decode patch " ++ p] model
-        Nothing -> runtimeError ["Not enough arguments!"] model
+pxcorOf = 
+  builtinPatchGetter (\v -> Stack.push (toString v.pxcor))
 
 {-|
 get the pycor of a patch at i, j and push it to stack 
 -}
 pycorOf : Argument -> Model -> Model
-pycorOf args model =
-  let
-    (i, j) = getCoords args model
-  in
-    if List.length args == 2 then
-      case Matrix.get i j model.patches of
-        Just v -> Stack.push (toString  v.pycor) model
-        Nothing -> incorrectCoords args model
-    else 
-      case List.head args of
-        Just p -> 
-          case patchFromString p of
-            Just v -> Stack.push (toString v.pycor) model
-            Nothing -> runtimeError ["Failed to decode patch " ++ p] model
-        Nothing -> runtimeError ["Not enough arguments!"] model
+pycorOf =
+  builtinPatchGetter (\v -> Stack.push (toString v.pycor))
+
 
 {-|
 get the pxycor of a patch at i, j and push it to stack 
 -}
 pxycorOf : Argument -> Model -> Model
-pxycorOf args model =
-  let
-    (i, j) = getCoords args model
-  in
-    if List.length args == 2 then
-      case Matrix.get i j model.patches of
-        Just v -> List.foldl Stack.push model <| List.reverse <| List.map toString [v.pxcor, v.pycor]
-        Nothing -> incorrectCoords args model
-    else 
-      case List.head args of
-        Just p -> 
-          case patchFromString p of
-            Just v -> List.foldl Stack.push model <| List.reverse <| List.map toString [v.pxcor, v.pycor]
-            Nothing -> runtimeError ["Failed to decode patch " ++ p] model
-        Nothing -> runtimeError ["Not enough arguments!"] model
+pxycorOf =
+  builtinPatchGetter (\v model -> List.foldl Stack.push model <| List.reverse <| List.map toString [v.pxcor, v.pycor])
