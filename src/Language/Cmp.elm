8ce550b763d755f3eec7918aea7e7c7aff936c91
eeue56 <enalicho@gmail.com>
2015-09-22 16:14:19 +0100

    make lt/mt expect ints

--- src/Language/Cmp.elm
+++ src/Language/Cmp.elm
@@ -3,11 +3,14 @@ module Language.Cmp where
 import Model exposing (..)
 import Parser.Errors exposing (runtimeError)
 import Language.Stack as Stack
+import Debug exposing (log)
+
+import Utils exposing (alwaysOkInt)
 
 cmpEngine : (String -> String -> Bool) -> Argument -> Model -> Model
 cmpEngine cmp args model =
   case args of 
-    a::b::[] -> Stack.push (toString <| cmp a b) model
+    a::b::[] -> Stack.push (toString <| log "comd" <|  cmp a b) model
     a::b::xs -> runtimeError ["Too many arguments for cmp!"] model
     a::[] -> runtimeError ["Only one argument provided.."] model
     [] -> runtimeError (["Not enough items in args for cmp!\nProvided:"] ++ args) model
@@ -28,25 +31,29 @@ notEq = cmpEngine (/=)
 less than args and push result
 -}
 lessThan : Argument -> Model -> Model 
-lessThan = cmpEngine (<)
+lessThan = 
+  cmpEngine (\x y -> alwaysOkInt x < alwaysOkInt y)
 
 {-| 
 less than args and push result
 -}
 lessThanOrEquals : Argument -> Model -> Model 
-lessThanOrEquals = cmpEngine (<=)
+lessThanOrEquals = 
+  cmpEngine (\x y -> alwaysOkInt x <= alwaysOkInt y)
 
 {-| 
 more than args and push result
 -}
 moreThan : Argument -> Model -> Model 
-moreThan = cmpEngine (>)
+moreThan = 
+  cmpEngine (\x y -> alwaysOkInt x > alwaysOkInt y)
 
 {-| 
 more than args and push result
 -}
 moreThanOrEquals : Argument -> Model -> Model 
-moreThanOrEquals = cmpEngine (>=)
+moreThanOrEquals = 
+  cmpEngine (\x y -> alwaysOkInt x >= alwaysOkInt y)
 
 true : Argument -> Model -> Model
 true args model = 
