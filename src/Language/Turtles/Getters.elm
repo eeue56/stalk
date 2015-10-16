bb18aa0b7e587c84245789e0a465311119ddc895
eeue56 <enalicho@gmail.com>
2015-10-16 14:04:20 +0100

    add case for just giving numbers

--- src/Language/Turtles/Getters.elm
+++ src/Language/Turtles/Getters.elm
@@ -8,6 +8,8 @@ import Language.Stack as Stack
 
 import Parser.Errors exposing (runtimeError)
 
+import String exposing (toInt)
+
 
 turtleAt : Int -> Int -> List Turtle -> Maybe Turtle
 turtleAt x y turtles =
@@ -19,6 +21,13 @@ turtleOn : Argument -> Model -> Model
 turtleOn args model =
   case args of 
     [] -> runtimeError ["Not enough arguments!"] model
+    x::y::[] -> 
+      case (toInt x, toInt y) of
+        (Ok x', Ok y') -> 
+          case turtleAt x' y' model.turtles of 
+            Nothing -> runtimeError ["No such turtle!"] model
+            Just v -> Stack.pushItem v .model
+        _ -> runtimeError ["Failed to convert arguments to int!"] model
     x::_ -> 
       case patchFromString x of
         Nothing -> runtimeError ["Failed to decode patch!"] model
